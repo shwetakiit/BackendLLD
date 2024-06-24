8b8b3416c786ae73ebbf5edce9d80b49f5ee9e11
shwetakiit <shweta0729e2open@gmail.com>
2024-06-24 19:02:39 +0530

    Builder Design

---
+++ src/kumari/shweta/DesignPattern/BuilderDesign/ClientStudent.java
@@ -0,0 +1,10 @@
+package kumari.shweta.DesignPattern.BuilderDesign;
+
+public class ClientStudent {
+
+    public static void main(String[] args) {
+        Student student = Student.getBuilder().setUnivName("KIIT").setName("SHK").setAge(25).build();
+        System.out.println(student);
+
+    }
+}
